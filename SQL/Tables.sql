258e88b75313c7491f3df9f7db2b6b1f2654fe2e
maxencemzn <maxence.mezin@hei.yncrea.fr>
2018-02-14 17:18:13 +0100

    modif fichier SQL

--- SQL/Tables.sql
+++ SQL/Tables.sql
@@ -1,19 +1,23 @@
+DROP TABLE IF EXISTS `faire`;
+DROP TABLE IF EXISTS `annonce`;
+DROP TABLE IF EXISTS `utilisateur`;
+
 CREATE TABLE `annonce` (
 
   `idAnnonce` int(11) NOT NULL AUTO_INCREMENT,
   `titre` varchar(100) NOT NULL,
   `description` varchar(1000) NOT NULL,
-  `date` date NOT NULL,
+  `dateDebut` date NOT NULL,
   `lieu` varchar(50) NOT NULL,
-  `formation` boolean NOT NULL,
+  `formation` tinyint NOT NULL,
   `tenue` varchar(100) NOT NULL,
-  `rémunneration` double(7,2) NOT NULL,
+  `remuneration` double(7,2) NOT NULL,
   
   PRIMARY KEY (`idAnnonce`),
   UNIQUE KEY `idAnnonce_UNIQUE` (`idAnnonce`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 
-CREATE TABLE `user` (
+CREATE TABLE `utilisateur` (
 
   `idUser` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) NOT NULL,
@@ -23,13 +27,14 @@ CREATE TABLE `user` (
   `tel` int(10) NOT NULL,
   `email` varchar(50) NOT NULL,
   `mdp` varchar(100) NOT NULL,
-  `admin` boolean NOT NULL,
+  `admin` tinyint NOT NULL,
   
   PRIMARY KEY (`idUser`),
   UNIQUE KEY `idUser_UNIQUE` (`idUser`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 
-CREATE TABLE `action` (
+
+CREATE TABLE `faire` (
 
   `idAnnonce` int(11) NOT NULL,
   `idUser` int(11) NOT NULL,
@@ -39,5 +44,5 @@ CREATE TABLE `action` (
   KEY `fk_idAnnonce` (`idAnnonce`),
   KEY `fk_idUser` (`idUser`),
   CONSTRAINT `fk_idAnnonce` FOREIGN KEY (`idAnnonce`) REFERENCES `annonce` (`idAnnonce`),
-  CONSTRAINT `fk_idUser` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`)
+  CONSTRAINT `fk_idUser` FOREIGN KEY (`idUser`) REFERENCES `utilisateur` (`idUser`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
