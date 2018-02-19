1498c3d2feafe90e727378d58b98b8a3d23e1803
maxencemzn <maxence.mezin@hei.yncrea.fr>
2018-02-19 19:43:36 +0100

    Chagement positionnement Footer (pas dans le projet Maven) + Implémentation servlets

---
+++ JobsNow/src/main/java/JobsNow/servlets/Page3PartenairesServlet.java
@@ -0,0 +1,31 @@
+package JobsNow.servlets;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/Partenaires")
+public class Page3PartenairesServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(req.getServletContext());
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setCharacterEncoding("UTF-8");
+
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+
+        templateEngine.process("Page3.Partenaires", context, resp.getWriter());
+    }
+}
